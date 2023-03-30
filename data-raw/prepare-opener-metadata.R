08cf7ca058ed31517812a2d52fb0f825fe6dc59f
Ben <statonbe@gmail.com>
2023-03-30 07:48:53 -0700

    Point to KuskoHarvUtils for utility functions

--- data-raw/prepare-opener-metadata.R
+++ data-raw/prepare-opener-metadata.R
@@ -4,8 +4,8 @@
 meta = read.csv("data-raw/opener-metadata.csv")
 
 # format start and end times for each opener
-meta$start = KuskoHarvEst:::combine_datetime(meta$date, meta$start_time)
-meta$end = KuskoHarvEst:::combine_datetime(meta$date, meta$end_time)
+meta$start = KuskoHarvUtils::combine_datetime(meta$date, meta$start_time)
+meta$end = KuskoHarvUtils::combine_datetime(meta$date, meta$end_time)
 
 # re-order and keep only relevant columns
 meta = meta[,c("start", "end", "flights_planned", "flights_flown", "announcement")]
