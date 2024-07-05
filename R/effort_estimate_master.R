7838e78f734f99ea08faefc46bc69d79ec5c6208
Ben <statonbe@gmail.com>
2024-07-05 07:55:16 -0700

    Update data set documentation
    Much more detail is now contained in the vignette. These now just point there and include the most basic information needed for user to understand what the data set is and where it came from.

--- R/effort_estimate_master.R
+++ R/effort_estimate_master.R
@@ -1,10 +1,16 @@
-#' Effort Estimates
+#' Compiled Effort Estimates
 #'
-#' A data set containing all compiled estimates of the number of
-#'   drift net trips that occurred in each subsistence harvest opportunity.
+#' Data set containing all compiled estimates of the number of drift gillnet trips
+#'   that occurred in each drift gillnet subsistence fishery opener, as estimated by
+#'   the Lower Kuskokwim River In-season Subsistence Salmon Harvest Monitoring Program.
 #'
-#'
-#' @format A data frame with rows representing a unique date- and area-specific effort estimate.
+#' @format See `vignette("datasets", package = "KuskoHarvData")`.
+#' @note This data set will be updated automatically when adding new interview and flight data,
+#'     see Steps 3 and 6 in `vignette("updating-data", package = "KuskoHarvData")`.
+#' @source The number of trips is estimated by combining aerial boat counts
+#'     (found in `data("flight_data_master", package = "KuskoHarvData"`) and
+#'     access point completed trip interviews (found in `data("interview_data_master")`).
+#'     The estimator is described in Staton et al. (In Review).
 #'
 
 "effort_estimate_master"
